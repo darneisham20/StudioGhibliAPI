3182a1afb5d7b3e87bc9ee8a7487904a0a707325
Darneisha Miller <darneisham20@gmail.com>
2023-05-28 16:40:47 -0400

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const baseURL = "https://ghibliapi.vercel.app/films"
+const containerDiv = document.getElementById('container')
+const searchDiv = document.getElementById('searchContainer')
+let searchInput = document.getElementById('search')
